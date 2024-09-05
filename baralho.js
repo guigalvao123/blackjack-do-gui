079769bff6c0544c3d2fe8d1a41ec5847428e21b
guigalvao123 <guilherme.galvaopadua@gmail.com>
2024-09-05 15:32:03 -0300

    criacao da logica do baralho.

---
+++ baralho.js
@@ -0,0 +1,22 @@
+function comprarCarta(){
+    const cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const naipes = ["♦️", "♥️", "♣️", "♠️"]
+    const sortearNumero = cartas[Math.floor(Math.random() * 13)]
+    const sortearNaipe = naipes[Math.floor(Math.random) * 4]
+    let valor
+
+    if(sortearNumero === "A"){
+        valor = 11
+    } else if(sortearNumero === "J" || sortearNumero === "Q" || sortearNumero === "K"){
+        valor = 10
+    } else{
+        valor = Number(sortearNumero)
+    }
+
+    const carta = {
+        texto: sortearNumero + sortearNaipe,
+        valor: valor
+    }
+
+    return carta
+}
